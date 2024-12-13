406f225dc64d424080d0ce5626caf3d10bf2e5d1
jchase2 <mr.jdchase@gmail.com>
2024-12-13 16:59:45 -0500

    Replaced some z stuff with a more flexible layout instead.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
+import { About } from "@/components/about";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex-1 self-center flex flex-col items-center">
-      <div className="-z-10">
-        <Image
-          src="/background.jpg"
-          alt="Background image"
-          layout="fill"
-          objectFit="cover"
-          priority={true}
-        />
-      </div>
-      <div className="absolute inset-x-0 top-[40%] md:top-[38%] lg:top-[38%] text-center space-y-2">
-        <h1 className="text-2xl text-slate-50">James D. Chase II</h1>
-        <h2 className="text-sm text-slate-50">
-          Developer Portfolio / Personal Site
-        </h2>
-      </div>
+    <div>
+      <section className="relative h-screen flex items-center justify-center bg-black">
+        <div className="absolute inset-0">
+          <Image
+            src="/background.jpg"
+            alt="Background image"
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </div>
+        <div className="relative text-center space-y-2 text-white -top-[5%] lg:-top-[10%]">
+          <h1 className="text-lg font-bold">James D. Chase II</h1>
+          <p className="text-sm">Developer Portfolio / Personal Site</p>
+        </div>
+      </section>
+      <section className="bg-gray-100 py-12">
+        <About />
+      </section>
     </div>
   );
 }
---
+++ src/components/about.tsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Skills: Front End: React, Redux, Angular, HTML5, CSS, and jQuery Back
+        End: Express, Koa, NodeJS, PostgreSQL, Sequelize, MongoDB, Mongoose,
+        REST APIs, .NET Core, MS SQL Server, Entity Framework Others:
+        JavaScript(ES6+), TypeScript, TDD/BDD, Jest, Cypress, JSON, Git, npm,
+        and JWT&apos;s I&apos;m a CS grad who&apos;s constantly improving my
+        skills. Recently, I’ve been working with React and its associated stack
+        on front end, as well as node with related tech on the back end. I have
+        experience building full stack apps and I&apos;m very interested in
+        growing my skills and taking on a new challenge. I have developed a wide
+        range of skills working on various projects and constantly expanding my
+        skills over the last few years. Feel free to check out some of my
+        projects! In my free time, I enjoy gaming, snowboarding riding
+        motorcycles and spending time with friends and family. I&apos;m always
+        open to trying new things!
+      </p>
+    </div>
+  );
+};
+
+export { About };
