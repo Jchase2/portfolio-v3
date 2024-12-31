b46ae53bb7e41c7e65634d740682eae81ac91893
jchase2 <mr.jdchase@gmail.com>
2024-12-31 15:30:46 -0500

    Making the default theme darkmode becuase it looks better imo.

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -6,7 +6,7 @@ type ThemeProviderProps = {
 
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
   return (
-    <BaseThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <BaseThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
     </BaseThemeProvider>
   );
